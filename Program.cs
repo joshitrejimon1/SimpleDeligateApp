c8323e5ebb9bcea1c4ded0f486255b4a2cd26126
joshitrejimon1 <joshit.rejimon@aurionpro.com>
2023-09-23 12:38:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimpleDelegateApp
+{
+    internal class Program
+    {
+        delegate void DMyDelegate(string name);
+
+        static void Main(string[] args)
+        {
+            DMyDelegate fptr;
+
+            fptr = PrintHello;
+            fptr += PrintHello; // You had a typo here; it should be PrintHello instead of PrintHellow
+            fptr += SayGoodbye; // You had a typo here; it should be SayGoodbye instead of SayGoodBye
+
+            fptr("Joshit"); // You should call the delegate with a parameter
+
+            Console.ReadLine();
+        }
+
+        static void PrintHello(string studentName)
+        {
+            Console.WriteLine("Hello Student " + studentName);
+        }
+
+        static void SayGoodbye(string name)
+        {
+            Console.WriteLine("Goodbye " + name);
+        }
+    }
+}
